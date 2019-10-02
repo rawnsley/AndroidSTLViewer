bd3fc94d7607d2c9dc8d787d90ca4ba49fc79040
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2019-10-02 14:28:32 +0100

    Ignore network thread exception

--- app/src/main/java/com/avn/stlviewer/MainActivity.kt
+++ app/src/main/java/com/avn/stlviewer/MainActivity.kt
@@ -2,8 +2,17 @@ package com.avn.stlviewer
 
 import android.app.Activity
 import android.os.Bundle
+import android.os.StrictMode
 
 class MainActivity : Activity() {
+
+    companion object {
+        init {
+            // Ignore thread policy
+            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
