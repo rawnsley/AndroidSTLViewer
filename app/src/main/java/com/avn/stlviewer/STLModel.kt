f767dd279778bd6759bfe469e1f78e1c914f893c
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2020-02-26 10:47:57 +0000

    Refactored OpenGL model into separate class

---
+++ app/src/main/java/com/avn/stlviewer/STLModel.kt
@@ -0,0 +1,260 @@
+package com.avn.stlviewer
+
+import android.app.Activity
+import android.opengl.GLES20
+import android.opengl.GLES31
+import android.opengl.Matrix
+import android.util.Log
+import com.avn.stlviewer.geometry.Vector3
+import java.io.File
+import java.io.RandomAccessFile
+import java.net.URL
+import java.nio.ByteOrder
+import java.nio.IntBuffer
+import java.nio.channels.FileChannel
+import kotlin.math.max
+
+class STLModel(activity: Activity) {
+
+    companion object {
+        private val TAG = STLModel::class.java.simpleName
+
+        // Small cube model
+//        private val TestModelUrl = URL("https://upload.wikimedia.org/wikipedia/commons/3/36/3D_model_of_a_Cube.stl")
+//        private const val TestModelSizeBytes = 684L
+
+        // Large model from https://www.cc.gatech.edu/projects/large_models/blade.html
+        // and converted to STL with MeshLab
+        private val TestModelUrl = URL("https://data.avncloud.com/dev/blade.stl")
+        private const val TestModelSizeBytes = 88269484L
+
+    }
+
+    private var modelRenderProgram : Int = 0
+    private var mvpMatrixHandle: Int = 0
+
+    private var vao : Int = 0
+    private var modelVertexBuffer : Int = 0
+    private var modelIndexBuffer : Int = 0
+    private var modelNormalBuffer : Int = 0
+    private var modelColorBuffer : Int = 0
+    private var modelTexCoordBuffer : Int = 0
+
+    private val mvpMatrix = FloatArray(16)
+    private val modelMatrix = FloatArray(16)
+
+    private var modelVertexCount = 0
+    private var modelIndexCount = 0
+
+    private var pendingAsset : STLAsset? = null
+
+    private var modelScale = 1f
+    private var modelTranslation : Vector3 = Vector3(0f, 0f, 0f)
+
+    init {
+
+        GLES31.glEnable(GLES31.GL_DEPTH_TEST)
+        GLES31.glClearColor(0.5f, 0.5f, 0.5f, 1.0f)
+
+        val intArray = IntArray(1)
+
+        // Load model render program
+
+        val vertShaderHandle = OpenGLUtil.compileShader(GLES31.GL_VERTEX_SHADER, activity.assets.open("shader.vert").bufferedReader().use { it.readText() })
+        val fragShaderHandle = OpenGLUtil.compileShader(GLES31.GL_FRAGMENT_SHADER, activity.assets.open("shader.frag").bufferedReader().use { it.readText() })
+
+        modelRenderProgram = GLES31.glCreateProgram()
+        GLES31.glAttachShader(modelRenderProgram, vertShaderHandle)
+        GLES31.glAttachShader(modelRenderProgram, fragShaderHandle)
+        OpenGLUtil.assertNoGlError()
+
+        // Link program
+        GLES31.glLinkProgram(modelRenderProgram)
+        OpenGLUtil.assertNoGlError()
+        // Check for compilation errors
+        GLES31.glGetProgramiv(modelRenderProgram, GLES31.GL_LINK_STATUS, intArray, 0)
+        if (intArray [0] == GLES31.GL_FALSE) {
+            throw Exception ("Error in linking program: ${GLES31.glGetProgramInfoLog(
+                modelRenderProgram
+            )}")
+        }
+        OpenGLUtil.assertNoGlError()
+
+        // Bind MVP matrix uniform
+        mvpMatrixHandle = GLES31.glGetUniformLocation(modelRenderProgram, "mvpMatrix")
+
+        // Shaders no longer required once the program is linked
+        GLES31.glDeleteShader(vertShaderHandle)
+        GLES31.glDeleteShader(fragShaderHandle)
+
+        // Start loading the STL file in the background
+
+        val thread = Thread {
+            val downloadFile = File(activity.cacheDir, "model.stl")
+            if(!downloadFile.exists() || downloadFile.length() != TestModelSizeBytes) {
+                Log.i(TAG, "Downloading model to ${downloadFile.path}")
+                com.avn.stlviewer.downloadFile(TestModelUrl, downloadFile)
+                Log.i(TAG, "Downloaded model file: ${downloadFile.length()} bytes")
+            } else {
+                Log.i(TAG, "Model already cached here ${downloadFile.path}")
+            }
+
+            val mBuffer = RandomAccessFile(downloadFile, "r").use { raf ->
+                raf.channel.use { channel ->
+                    channel.map(FileChannel.MapMode.READ_ONLY, 0, downloadFile.length())
+                }
+            }
+            mBuffer.order(ByteOrder.LITTLE_ENDIAN)
+
+            Log.i("TAG", "STL load started")
+            pendingAsset = STLAsset(mBuffer)
+            Log.i("TAG", "STL load complete")
+        }
+        thread.start()
+    }
+
+    fun loadGeometryOnGpu(asset : STLAsset) {
+        Log.i("TAG", "Loading asset geometry with ${asset.triangleCount} triangles")
+
+        modelVertexCount = asset.vertexCount
+        modelIndexCount = asset.triangleCount * 3
+
+        val intBuffer = IntBuffer.allocate(1)
+
+        GLES31.glGenVertexArrays(1, intBuffer)
+        vao = intBuffer[0]
+
+        GLES31.glGenBuffers(1, intBuffer)
+        modelVertexBuffer = intBuffer[0]
+
+        GLES31.glGenBuffers(1, intBuffer)
+        modelIndexBuffer = intBuffer[0]
+
+        GLES31.glGenBuffers(1, intBuffer)
+        modelNormalBuffer = intBuffer[0]
+
+        GLES31.glGenBuffers(1, intBuffer)
+        modelColorBuffer = intBuffer[0]
+
+        GLES31.glGenBuffers(1, intBuffer)
+        modelTexCoordBuffer = intBuffer[0]
+
+        GLES31.glBindVertexArray(vao)
+
+        // Verticies
+
+        GLES31.glBindBuffer(GLES31.GL_ARRAY_BUFFER, modelVertexBuffer)
+        GLES31.glBufferData(
+            GLES31.GL_ARRAY_BUFFER,
+            modelVertexCount * 3 * 4,
+            asset.verts,
+            GLES31.GL_STATIC_DRAW
+        )
+        val vPosition = GLES31.glGetAttribLocation(modelRenderProgram, "vPosition")
+        GLES31.glEnableVertexAttribArray(vPosition)
+        GLES31.glVertexAttribPointer(vPosition, 3, GLES31.GL_FLOAT, false, 0, 0)
+
+        // Normals
+
+        GLES31.glBindBuffer(GLES31.GL_ARRAY_BUFFER, modelNormalBuffer)
+        GLES31.glBufferData(
+            GLES31.GL_ARRAY_BUFFER,
+            modelVertexCount * 3 * 4,
+            asset.norms,
+            GLES31.GL_STATIC_DRAW
+        )
+        val vNormal = GLES31.glGetAttribLocation(modelRenderProgram, "vNormal")
+        GLES31.glEnableVertexAttribArray(vNormal)
+        GLES31.glVertexAttribPointer(vNormal, 3, GLES31.GL_FLOAT, false, 0, 0)
+
+        // Colors (from normals)
+
+        GLES31.glBindBuffer(GLES31.GL_ARRAY_BUFFER, modelColorBuffer)
+        GLES31.glBufferData(
+            GLES31.GL_ARRAY_BUFFER,
+            modelVertexCount * 3 * 4,
+            asset.norms,
+            GLES31.GL_STATIC_DRAW
+        )
+        val vColor = GLES31.glGetAttribLocation(modelRenderProgram, "vColor")
+        GLES31.glEnableVertexAttribArray(vColor)
+        GLES31.glVertexAttribPointer(vColor, 3, GLES31.GL_FLOAT, false, 0, 0)
+
+        // TexCoords (from normals)
+
+        GLES31.glBindBuffer(GLES31.GL_ARRAY_BUFFER, modelTexCoordBuffer)
+        GLES31.glBufferData(
+            GLES31.GL_ARRAY_BUFFER,
+            modelVertexCount * 2 * 4,
+            asset.norms,
+            GLES31.GL_STATIC_DRAW
+        )
+        val vTexCoord = GLES31.glGetAttribLocation(modelRenderProgram, "vTexCoord")
+        GLES31.glEnableVertexAttribArray(vTexCoord)
+        GLES31.glVertexAttribPointer(vTexCoord, 2, GLES31.GL_FLOAT, false, 0, 0)
+
+        // Indicies
+
+        GLES31.glBindBuffer(GLES31.GL_ELEMENT_ARRAY_BUFFER, modelIndexBuffer)
+        GLES31.glBufferData(
+            GLES31.GL_ELEMENT_ARRAY_BUFFER,
+            modelIndexCount * 4,
+            asset.indxs,
+            GLES31.GL_STATIC_DRAW
+        )
+
+        GLES31.glBindVertexArray(0)
+
+        // Set translation and scale factors to get the model in the middle of the view
+        modelTranslation = -asset.bounds.center
+        val maxDimension = max(max(asset.bounds.size.x, asset.bounds.size.y), asset.bounds.size.z)
+        modelScale = 1f / maxDimension
+    }
+
+    fun render(displayTimeNs : Long, viewMatrix : FloatArray, projectionMatrix : FloatArray) {
+        // Load the STL asset if it's ready
+        pendingAsset?.let {
+            loadGeometryOnGpu(it)
+            pendingAsset = null
+        }
+
+        // Position, scale, and spin the model
+
+        Matrix.setIdentityM(modelMatrix, 0)
+        Matrix.translateM(modelMatrix, 0, 0f, 0f, -1f)
+        Matrix.rotateM(
+            modelMatrix,
+            0,
+            (displayTimeNs.toDouble() / 49_997_117.0).toFloat(),
+            1f,
+            0.5f,
+            0f
+        )
+        Matrix.scaleM(modelMatrix, 0, modelScale, modelScale, modelScale)
+        Matrix.translateM(
+            modelMatrix,
+            0,
+            modelTranslation.x,
+            modelTranslation.y,
+            modelTranslation.z
+        )
+
+        // Create combined MVP
+
+        Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
+        Matrix.multiplyMM(mvpMatrix, 0, mvpMatrix, 0, modelMatrix, 0)
+
+        // Render model
+
+        GLES31.glUseProgram(modelRenderProgram)
+        GLES31.glUniformMatrix4fv(mvpMatrixHandle, 1, false, mvpMatrix, 0)
+        GLES31.glBindVertexArray(vao)
+        GLES31.glDrawElements(GLES31.GL_TRIANGLES, modelIndexCount, GLES31.GL_UNSIGNED_INT, 0)
+        GLES31.glBindVertexArray(0)
+
+        // Force GL instructions to finish so texture is complete for submission to SxrAPI
+        GLES20.glFinish()
+
+    }
+
+}
--- app/src/main/java/com/avn/stlviewer/STLRenderer.kt
+++ app/src/main/java/com/avn/stlviewer/STLRenderer.kt
@@ -8,29 +8,12 @@ import android.util.Log
 import android.view.Choreographer
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-import com.avn.stlviewer.geometry.Vector3
-import java.io.File
-import java.io.RandomAccessFile
-import java.net.URL
-import java.nio.ByteOrder
-import java.nio.IntBuffer
-import java.nio.channels.FileChannel
-import kotlin.math.max
 
 
 class STLRenderer(val activity : Activity, val surfaceView: SurfaceView) : SurfaceHolder.Callback {
 
     companion object {
         private val TAG = STLRenderer::class.java.simpleName
-
-        // Small cube model
-//        private val TestModelUrl = URL("https://upload.wikimedia.org/wikipedia/commons/3/36/3D_model_of_a_Cube.stl")
-//        private const val TestModelSizeBytes = 684L
-
-        // Large model from https://www.cc.gatech.edu/projects/large_models/blade.html
-        // and converted to STL with MeshLab
-        private val TestModelUrl = URL("https://data.avncloud.com/dev/blade.stl")
-        private const val TestModelSizeBytes = 88269484L
 
         //TODO: WHAT VALUES TO USE HERE?
         val RenderTextureWidth = 1280
@@ -51,39 +34,18 @@ class STLRenderer(val activity : Activity, val surfaceView: SurfaceView) : Surfa
     private val renderColorTextures = IntArray(FrameBufferCount)
     private val renderDepthTextures = IntArray(FrameBufferCount)
 
-    private var modelRenderProgram : Int = 0
-    private var mvpMatrixHandle: Int = 0
-
-    private var vao : Int = 0
-    private var modelVertexBuffer : Int = 0
-    private var modelIndexBuffer : Int = 0
-    private var modelNormalBuffer : Int = 0
-    private var modelColorBuffer : Int = 0
-    private var modelTexCoordBuffer : Int = 0
-
-    private val mvpMatrix = FloatArray(16)
-    private val modelMatrix = FloatArray(16)
-    private val projectionMatrix = FloatArray(16)
-
-    private var modelVertexCount = 0
-    private var modelIndexCount = 0
-
-    private var pendingAsset : STLAsset? = null
-
-    private var modelScale = 1f
-    private var modelTranslation : Vector3 = Vector3(0f, 0f, 0f)
-
     // Choreographer is used to schedule new frames
     private val choreographer: Choreographer = Choreographer.getInstance()
     // Performs the rendering and schedules new frames
     private val frameScheduler = FrameCallback()
 
+    private val projectionMatrix = FloatArray(16)
+
+    private lateinit var stlModel : STLModel
+
     init {
         surfaceView.holder.addCallback(this)
     }
-
-    var pendingResume = false
-    var pendingPause = false
 
     fun resume() {
         Log.i(TAG, "resume")
@@ -98,7 +60,7 @@ class STLRenderer(val activity : Activity, val surfaceView: SurfaceView) : Surfa
     }
 
     fun focusChanged(hasFocus : Boolean) {
-
+        //TODO: Pause rendering
     }
 
     fun destroy() {
@@ -112,7 +74,12 @@ class STLRenderer(val activity : Activity, val surfaceView: SurfaceView) : Surfa
 
         eglManager = EGLManager(surfaceView)
 
-        loadOpenGL()
+        val ratio: Float = STLRenderer.RenderTextureWidth.toFloat() / STLRenderer.RenderTextureHeight.toFloat()
+        Matrix.perspectiveM(projectionMatrix, 0, STLRenderer.VerticalFoV, ratio, 0.1f, 1_000f)
+
+        createSwapChain()
+
+        stlModel = STLModel(activity)
     }
 
     override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
@@ -126,8 +93,7 @@ class STLRenderer(val activity : Activity, val surfaceView: SurfaceView) : Surfa
     }
 
 
-    //RESUSE DEPTH?
-    fun loadOpenGL() {
+    fun createSwapChain() {
 
         glEnable(GL_DEPTH_TEST)
         glClearColor(0.5f, 0.5f, 0.5f, 1.0f)
@@ -170,134 +136,6 @@ class STLRenderer(val activity : Activity, val surfaceView: SurfaceView) : Surfa
 
             OpenGLUtil.assertNoGlError()
         }
-
-        // Load model render program
-
-        val vertShaderHandle = OpenGLUtil.compileShader(GL_VERTEX_SHADER, activity.assets.open("shader.vert").bufferedReader().use { it.readText() })
-        val fragShaderHandle = OpenGLUtil.compileShader(GL_FRAGMENT_SHADER, activity.assets.open("shader.frag").bufferedReader().use { it.readText() })
-
-        modelRenderProgram = glCreateProgram()
-        glAttachShader(modelRenderProgram, vertShaderHandle)
-        glAttachShader(modelRenderProgram, fragShaderHandle)
-        OpenGLUtil.assertNoGlError()
-
-        // Link program
-        glLinkProgram(modelRenderProgram)
-        OpenGLUtil.assertNoGlError()
-        // Check for compilation errors
-        glGetProgramiv (modelRenderProgram, GL_LINK_STATUS, intArray, 0)
-        if (intArray [0] == GL_FALSE) {
-            throw Exception ("Error in linking program: ${glGetProgramInfoLog (modelRenderProgram)}")
-        }
-        OpenGLUtil.assertNoGlError()
-
-        // Bind MVP matrix uniform
-        mvpMatrixHandle = glGetUniformLocation(modelRenderProgram, "mvpMatrix")
-
-        // Shaders no longer required once the program is linked
-        glDeleteShader(vertShaderHandle)
-        glDeleteShader(fragShaderHandle)
-
-        val ratio: Float = RenderTextureWidth.toFloat() / RenderTextureHeight.toFloat()
-        Matrix.perspectiveM(projectionMatrix, 0, VerticalFoV, ratio, 0.1f, 1_000f)
-
-        // Start loading the STL file in the background
-
-        val thread = Thread {
-            val downloadFile = File(activity.cacheDir, "model.stl")
-            if(!downloadFile.exists() || downloadFile.length() != TestModelSizeBytes) {
-                Log.i(TAG, "Downloading model to ${downloadFile.path}")
-                downloadFile(TestModelUrl, downloadFile)
-                Log.i(TAG, "Downloaded model file: ${downloadFile.length()} bytes")
-            } else {
-                Log.i(TAG, "Model already cached here ${downloadFile.path}")
-            }
-
-            val mBuffer = RandomAccessFile(downloadFile, "r").use { raf ->
-                raf.channel.use { channel ->
-                    channel.map(FileChannel.MapMode.READ_ONLY, 0, downloadFile.length())
-                }
-            }
-            mBuffer.order(ByteOrder.LITTLE_ENDIAN)
-
-            Log.i("TAG", "STL load started")
-            pendingAsset = STLAsset(mBuffer)
-            Log.i("TAG", "STL load complete")
-        }
-        thread.start()
-    }
-
-    fun loadGeometryOnGpu(asset : STLAsset) {
-        Log.i("TAG", "Loading asset geometry with ${asset.triangleCount} triangles")
-
-        modelVertexCount = asset.vertexCount
-        modelIndexCount = asset.triangleCount * 3
-
-        val intBuffer = IntBuffer.allocate(1)
-
-        glGenVertexArrays(1, intBuffer)
-        vao = intBuffer[0]
-
-        glGenBuffers(1, intBuffer)
-        modelVertexBuffer = intBuffer[0]
-
-        glGenBuffers(1, intBuffer)
-        modelIndexBuffer = intBuffer[0]
-
-        glGenBuffers(1, intBuffer)
-        modelNormalBuffer = intBuffer[0]
-
-        glGenBuffers(1, intBuffer)
-        modelColorBuffer = intBuffer[0]
-
-        glGenBuffers(1, intBuffer)
-        modelTexCoordBuffer = intBuffer[0]
-
-        glBindVertexArray(vao)
-
-        // Verticies
-
-        glBindBuffer(GL_ARRAY_BUFFER, modelVertexBuffer)
-        glBufferData(GL_ARRAY_BUFFER, modelVertexCount * 3 * 4, asset.verts, GL_STATIC_DRAW)
-        val vPosition = glGetAttribLocation(modelRenderProgram, "vPosition")
-        glEnableVertexAttribArray(vPosition)
-        glVertexAttribPointer(vPosition, 3, GL_FLOAT, false, 0, 0)
-
-        // Normals
-
-        glBindBuffer(GL_ARRAY_BUFFER, modelNormalBuffer)
-        glBufferData(GL_ARRAY_BUFFER, modelVertexCount * 3 * 4, asset.norms, GL_STATIC_DRAW)
-        val vNormal = glGetAttribLocation(modelRenderProgram, "vNormal")
-        glEnableVertexAttribArray(vNormal)
-        glVertexAttribPointer(vNormal, 3, GL_FLOAT, false, 0, 0)
-
-        // Colors (from normals)
-
-        glBindBuffer(GL_ARRAY_BUFFER, modelColorBuffer)
-        glBufferData(GL_ARRAY_BUFFER, modelVertexCount * 3 * 4, asset.norms, GL_STATIC_DRAW)
-        val vColor = glGetAttribLocation(modelRenderProgram, "vColor")
-        glEnableVertexAttribArray(vColor)
-        glVertexAttribPointer(vColor, 3, GL_FLOAT, false, 0, 0)
-
-        // TexCoords (from normals)
-
-        glBindBuffer(GL_ARRAY_BUFFER, modelTexCoordBuffer)
-        glBufferData(GL_ARRAY_BUFFER, modelVertexCount * 2 * 4, asset.norms, GL_STATIC_DRAW)
-        val vTexCoord = glGetAttribLocation(modelRenderProgram, "vTexCoord")
-        glEnableVertexAttribArray(vTexCoord)
-        glVertexAttribPointer(vTexCoord, 2, GL_FLOAT, false, 0, 0)
-
-        // Indicies
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, modelIndexBuffer)
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, modelIndexCount * 4, asset.indxs, GL_STATIC_DRAW)
-
-        glBindVertexArray(0)
-
-        // Set translation and scale factors to get the model in the middle of the view
-        modelTranslation = -asset.bounds.center
-        val maxDimension = max(max(asset.bounds.size.x, asset.bounds.size.y), asset.bounds.size.z)
-        modelScale = 1f / maxDimension
     }
 
     inner class FrameCallback : Choreographer.FrameCallback {
@@ -311,15 +149,10 @@ class STLRenderer(val activity : Activity, val surfaceView: SurfaceView) : Surfa
         override fun doFrame(frameTimeNanos: Long) {
 
             try {
-                pendingAsset?.let {
-                    loadGeometryOnGpu(it)
-                    pendingAsset = null
-                }
-
                 if (sxrManager.isReady) {
 
                     // Select alternating texture buffers
-                    val bufferIndex = bufferIndex++ % FrameBufferCount
+                    bufferIndex = ++bufferIndex % FrameBufferCount
 
                     glBindFramebuffer(GL_FRAMEBUFFER, frameBufferHandles[bufferIndex])
 
@@ -329,50 +162,13 @@ class STLRenderer(val activity : Activity, val surfaceView: SurfaceView) : Surfa
 
                     val headPoseState = sxrManager.startFrame(renderColorTextures[bufferIndex],  renderColorTextures[bufferIndex])
                     if(headPoseState.isValid()) {
-                        // Set the camera position
+                        // Get camera position from head pose
                         val viewMatrix = headPoseState.pose.rotation.quatToMatrix().queueInArray()
 
-                        // Render model
-
-                        glUseProgram(modelRenderProgram)
-
-                        // Combine the projection and camera view transformation
-                        Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
-
-                        // Position and spin the model
-                        Matrix.setIdentityM(modelMatrix, 0)
-                        Matrix.translateM(modelMatrix, 0, 0f, 0f, -1f)
-                        Matrix.rotateM(
-                            modelMatrix,
-                            0,
-                            (headPoseState.expectedDisplayTimeNs.toDouble() / 49_997_117.0).toFloat(),
-                            1f,
-                            0.5f,
-                            0f
-                        )
-                        Matrix.scaleM(modelMatrix, 0, modelScale, modelScale, modelScale)
-                        Matrix.translateM(
-                            modelMatrix,
-                            0,
-                            modelTranslation.x,
-                            modelTranslation.y,
-                            modelTranslation.z
-                        )
-
-                        // Create final MVP and set in program
-                        Matrix.multiplyMM(mvpMatrix, 0, mvpMatrix, 0, modelMatrix, 0)
-                        glUniformMatrix4fv(mvpMatrixHandle, 1, false, mvpMatrix, 0)
-
-                        // Draw model
-                        glBindVertexArray(vao)
-                        glDrawElements(GL_TRIANGLES, modelIndexCount, GL_UNSIGNED_INT, 0)
-                        glBindVertexArray(0)
-
-                        GLES20.glFinish()
+                        stlModel.render(headPoseState.expectedDisplayTimeNs, viewMatrix, projectionMatrix)
                     }
                     sxrManager.endFrame()
 
-//Thread.sleep(1000)
                     ++renderedFrames
 
                 }
